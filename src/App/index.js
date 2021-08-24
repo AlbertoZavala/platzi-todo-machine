1e6f0aa4fc79fcd1071a0cba1d522452f65f616d
Alberto Zavala <jzavala@efim.com>
2021-08-24 08:57:51 -0700

    Se creo custom hooks

--- src/App/index.js
+++ src/App/index.js
@@ -7,19 +7,34 @@ import { AppUI } from "./AppUI";
 //   { text: "Llorar con la Llorona", completed: true },
 // ];
 
-function App() {
-  const localStorageTodos = localStorage.getItem('TODOS_V1');
+function useLocalStorage(itemName, initialValue) {
+  const localStorageItem = localStorage.getItem(itemName);
+  let parsedItem;
 
-  let parsedTodos;
-
-  if(!localStorageTodos){
-    localStorage.setItem('TODOS_V1', JSON.stringify([]));
-    parsedTodos = [];
-  }else{
-    parsedTodos = JSON.parse(localStorageTodos);
+  if (!localStorageItem) {
+    localStorage.setItem(itemName, JSON.stringify(initialValue));
+    parsedItem = initialValue;
+  } else {
+    parsedItem = JSON.parse(localStorageItem);
   }
 
-  const [todos, setTodos] = React.useState(parsedTodos);
+  const [items, setItems] = React.useState(parsedItem);
+
+  const saveItems = (newItems) => {
+    const stringifiedItems = JSON.stringify(newItems);
+    localStorage.setItem(itemName, stringifiedItems);
+    setItems(newItems);    
+  }
+
+  return [
+    items,
+    saveItems
+  ];
+}
+
+
+function App() {
+  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);  
   const [searchValue, setSearchValue] = React.useState('');
 
   const completedTodos = todos.filter(todo => !!todo.completed).length;
@@ -35,11 +50,7 @@ function App() {
     });
   }
 
-  const saveTodos = (newTodos) => {
-    const stringifiedTodos = JSON.stringify(newTodos);
-    localStorage.setItem('TODOS_V1', stringifiedTodos);
-    setTodos(newTodos);    
-  }
+  
 
   const completeTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text);
