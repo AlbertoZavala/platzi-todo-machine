3481bf0d9c7ecd067486cbf81dc3d12ec327485b
Jose Alberto Zavala Flores <jose.alberto.zavala.flores@gmail.com>
2021-08-23 21:35:41 -0700

    Se agregaron filtros

--- src/App.js
+++ src/App.js
@@ -6,20 +6,43 @@ import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
 //import './App.css';
 
-const todos = [
+const defaultTodos = [
   { text: "Cortar Cebolla", completed: true },
   { text: "Tomar Curso de intro a React", completed: false },
-  { text: "Llorar con la Llorona", completed: false },
+  { text: "Llorar con la Llorona", completed: true },
 ];
 
 function App() {
+  const [todos, setTodos] = React.useState(defaultTodos);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const totalTodos = todos.length;
+
+  let searchedTodos =  [];
+
+  if(!searchValue.length > 0){
+    searchedTodos = todos;
+  }else{
+    searchedTodos = todos.filter((todo) => {
+      return todo.text.toLowerCase().includes(searchValue.toLowerCase())
+    });
+  }
+
   return (
     <React.Fragment>
-      <TodoCounter />
-      <TodoSearch />
+      <TodoCounter 
+        total={totalTodos}
+        completed={completedTodos}
+      />
+
+      <TodoSearch
+        searchValue = {searchValue}
+        setSearchValue = {setSearchValue}
+      />
 
       <TodoList>
-        {todos.map((todo) => (
+        {searchedTodos.map((todo) => (
           <TodoItem 
             key={todo.text} 
             text={todo.text} 
